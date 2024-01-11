57efc9b1c1d381a03615e3af629694550f96d730
ihongjinlee <ihongjinlee@gmail.com>
2024-01-11 11:05:14 +0900

    [TODOLIST] 06.TodoItem 만들기 close #6

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray; /* 사이즈 조정 확인용 */
 `;
 
 function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="할 일 하나" done={true} />
+      <TodoItem text="할 일 둘" done={true} />
+      <TodoItem text="할 일 셋" done={false} />
+      <TodoItem text="할 일 넷" done={false} />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
