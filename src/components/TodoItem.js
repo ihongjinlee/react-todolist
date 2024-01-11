09568b119089350ef9571d7c2ea3856bdc590a17
ihongjinlee <ihongjinlee@gmail.com>
2024-01-11 15:00:08 +0900

    [TODOLIST] 16.TodoItem 기능 구현 완성하기 - React.memo로 최적화 close #16

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import { MdDone, MdDelete } from 'react-icons/md';
 import { useTodoDispatch } from './TodoContext';
+import React from 'react';
 
 const Remove = styled.div`
   display: flex;
@@ -75,4 +76,4 @@ function TodoItem({ id, done, text }) {
   );
 }
 
-export default TodoItem;
+export default React.memo(TodoItem);
